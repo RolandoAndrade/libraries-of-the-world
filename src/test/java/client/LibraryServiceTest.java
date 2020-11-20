ab1dd0b5f97bd7692918d1d9d4e63a5c7a72bb10
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-11-20 09:21:19 -0400

    Tests

--- src/test/java/client/LibraryServiceTest.java
+++ src/test/java/client/LibraryServiceTest.java
@@ -1,21 +1,46 @@
 package client;
 
+import client.application.LibraryService;
+import client.domain.BookNotFoundException;
 import client.domain.LibraryRepository;
-import shared.domain.LoggerService;
+import org.junit.Before;
+import org.junit.Test;
+import shared.domain.Book;
+import shared.infrastructure.ConsoleLogger;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
 
 public class LibraryServiceTest {
+
     private LibraryRepository repository;
-    private LoggerService loggerService;
+    private LibraryService service;
 
-    @org.junit.Before
+    @Before
     public void setup(){
+        this.repository = mock(LibraryRepository.class);
+        this.service = new LibraryService(this.repository, new ConsoleLogger());
     }
 
-    @org.junit.Test
-    public void getBook() {
+    @Test
+    public void getBook() throws BookNotFoundException {
+        String title = "Implementing Domain Driven Design";
+        when(this.repository.getBook(title)).thenReturn(new Book(title));
+        Book book = this.service.getBook(title);
+        assertEquals(title, book.getTitle());
+
     }
 
-    @org.junit.Test
+    @Test(expected=BookNotFoundException.class)
+    public void getBookThatCannotBeFound() throws BookNotFoundException {
+        String title = "Harry Potter";
+        when(this.repository.getBook(title)).thenReturn(null);
+        Book book = this.service.getBook(title);
+    }
+
+
+    @Test
     public void getAuthor() {
     }
 }
